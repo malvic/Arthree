4a50dbc3f428891bf4337a4d996cadf93a25e606
malvic <mrizqischlproject@gmail.com>
2026-08-02 15:06:00 +0700

    Add files via upload

---
+++ datagalery11e.js
@@ -0,0 +1,25 @@
+const galleryData = [
+  {
+    "image": "Fotbarkelas11e.jpeg",
+    "title": "Permulaan",
+    "desc": `Menyongsong Masa Depan: Langkah Awal XI-E MAN 1 Kota Malang. 
+Foto ini menandai berdirinya keluarga baru kami, kelas XI-E. 
+Dengan fondasi keimanan dan ilmu yang kami timba di MAN 1 Kota Malang, 
+kami berkomitmen untuk menjadi angkatan yang berprestasi, berkarakter, dan inovatif. 
+Ini adalah awal dari halaman baru penuh harapan, di mana kami akan saling mendukung untuk meraih cita-cita tertinggi. 
+Saksikan perjalanan kami!`
+  }, // <--- Tanda penutup ini dan koma sebelumnya hilang di kode kamu
+
+  {
+    "image": "fotbarupacara2.jpeg",
+    "title": "Tiba - Tiba upacara",
+    "desc": `foto ini diambil pada tanggal 29 bulan 29 juli 2026 hari rabu pada pagi hari
+    setelah upacara bendera di lapangan outdoor MAN 1 Kota Malang`
+  },
+  
+  {
+    "image": "https://picsum.photos/id/1025/1200/600",
+    "title": "Anjing Lucu",
+    "desc": "Foto ketiga menampilkan seekor anjing yang sedang bersantai di selimut hangat."
+  }
+];
